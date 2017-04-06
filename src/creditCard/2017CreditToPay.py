9e3151ddd05b12669af1c5e9dce7f2e9a688b867
yanghengx <yanghengx@nonobank.com>
2017-04-06 11:27:54 +0800

    增加2017.03月份的信用卡

--- src/creditCard/2017CreditToPay.py
+++ src/creditCard/2017CreditToPay.py
@@ -1,6 +1,7 @@
 def need_to_pay(yearmonth, my, pf, zs):
     print(yearmonth + ' credit card cost: ', round(my + pf + zs, 2))
 
+
 # 2016.12
 my = 3901.5
 pf = 1204.0
@@ -18,3 +19,9 @@ my = 1586.88
 pf = 527.0
 zs = 4643.27
 need_to_pay('2017.02', my, pf, zs)
+
+# 2017.03
+my = 1464.51
+pf = 527.98
+zs = 859.97
+need_to_pay('2017.03', my, pf, zs)
