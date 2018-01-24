2cd7b991b90aff4259aeae80f6d38b170ac1ad6d
yanghengx <yanghengx@nonobank.com>
2018-01-24 19:13:35 +0800

    add 0801 card

--- src/creditCard/2018_credit_card.py
+++ src/creditCard/2018_credit_card.py
@@ -2,6 +2,6 @@ from src.creditCard import pay_computes as pc
 
 # 2018.01
 my = 0
-pf = 0
+pf = 2592.20
 zs = 0
-pay01 = pc.need_to_pay('2017.12', my, pf, zs)
+pay01 = pc.need_to_pay('2018.01', my, pf, zs)
