65fc32aa630e2c0ed79609d8b11ef473e467ec63
yanghengx <yanghengx@nonobank.com>
2018-03-07 09:43:09 +0800

    201802 pay

--- src/creditCard/2018_credit_card.py
+++ src/creditCard/2018_credit_card.py
@@ -5,3 +5,11 @@ my = 2361.9
 pf = 2592.20
 zs = 108.55
 pay01 = pc.need_to_pay('2018.01', my, pf, zs)
+
+# 2018.02
+my = 1403.01
+pf = 1596
+zs = 4353
+pay02 = pc.need_to_pay('2018.02', my, pf, zs)
+
+
