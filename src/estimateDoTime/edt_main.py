a95081e2eae04ecabf005ef19dc48138f4b55cbc
yanghengx <yanghengx@nonobank.com>
2018-04-10 15:05:55 +0800

    修改近期技术书单

--- src/estimateDoTime/edt_main.py
+++ src/estimateDoTime/edt_main.py
@@ -2,7 +2,7 @@
 from estimateDoTime import estimate_time as est
 
 
-def add_recently_books():
+def add_it_books():
     """
     add books
     :return:
@@ -12,9 +12,12 @@ def add_recently_books():
     books['Zookeeper技术详解'] = 200
     books['SpringCloud实战'] = 417
     books['聊聊架构'] = 230
-    books['Http权威指南'] = 518
+    # books['Http权威指南'] = 518
+    books['高性能Mysql'] = 800
     return books
 
 
-cost_hours = est.cost_time(add_recently_books())
+cost_hours = est.cost_time(add_it_books())
 est.cost_days(cost_hours)
+
+
