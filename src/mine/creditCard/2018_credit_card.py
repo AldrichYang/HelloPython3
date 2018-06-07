fe63756f6080a1061e9f9be57c4918fb856c551f
yanghengx <yanghengx@nonobank.com>
2018-06-07 14:10:02 +0800

    2018.05 credit

--- src/mine/creditCard/2018_credit_card.py
+++ src/mine/creditCard/2018_credit_card.py
@@ -24,6 +24,13 @@ pf = 861.1
 zs = 361
 pay04 = pc.need_to_pay('2018.04', my, pf, zs)
 
+# 2018.05
+my = 1495.58
+pf = 1085
+zs = 1735.5
+ms = 591.4
+pay05 = pc.need_to_pay('2018.05', my, pf, zs, ms)
+
 sum_pay = [pay01, pay02, pay03, pay04]
 
 print(round(sum(sum_pay), 2))
