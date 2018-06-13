104ce901b0ec4316027a57e29f7019b81031d886
yanghengx <yanghengx@nonobank.com>
2018-06-13 10:40:07 +0800

    数字比较

--- src/language/mathematics/DecimalLearn.py
+++ src/language/mathematics/DecimalLearn.py
@@ -1,3 +1,8 @@
 from decimal import *
+
 getcontext().prec = 6
 print(Decimal(1.35) + Decimal(6.15))
+print(Decimal(419.99) == float(419.99))
+print(Decimal(35) == float(35.0))
+print(Decimal(419.99).__float__().hex())
+print(float(419.99).hex())
