732c751879a936b43574e748f8e12bb7fdf0a085
yanghengx <yanghengx@nonobank.com>
2019-03-08 10:10:41 +0800

    201902 credit cost

--- src/mine/creditCard/2019_credit_card.py
+++ src/mine/creditCard/2019_credit_card.py
@@ -5,9 +5,15 @@ my = 1384.42
 pf = 636.85
 zs = 1776.03
 ms = 364.1
-pay01 = pc.need_to_pay('2019.01', my, pf, zs)
+pay01 = pc.need_to_pay('2019.01', my, pf, zs, ms)
 
+# 2019.02
+ms = 308.5
+my = 1062.65
+zs = 597.65
+pf = 3158.2
+pay02 = pc.need_to_pay('2019.02', my, pf, zs, ms)
 
-sum_pay = [pay01]
+sum_pay = [pay01, pay02]
 
 print(round(sum(sum_pay), 2))
