25b390d79c10a5667a0eb931fbd04fa8e5d71ddc
yanghengx <yanghengx@nonobank.com>
2019-04-26 11:15:40 +0800

    add invset calculator

--- src/temp.py
+++ src/temp.py
@@ -11,3 +11,13 @@ for each in each_year_add:
 print(earn_after_20_year)
 
 print(earn_after_20_year * math.pow(1.05, 5))
+
+
+rate_list = [7.20, 7.40, 7.60, 7.80, 8.00,
+             8.20, 8.40, 8.60, 8.80, 9.00, 9.20, 9.40]
+amount = 1000
+days = 415
+profit = []
+[profit.append(round(amount * 30 * each / 100 / 365,2)) for each in rate_list]
+print(sum(profit) + round(amount * (days%(30*12))*9.4/100/365,2))
+print(pow(2,32))
