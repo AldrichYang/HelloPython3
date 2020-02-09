73164eaa82e053aef43aff3f13c7bcbb0fd38d6e
杨恒 <yanghengx@nonobank.com>
2020-02-09 13:50:22 +0800

    202001

--- src/mine/creditCard/2020_credit_card.py
+++ src/mine/creditCard/2020_credit_card.py
@@ -2,9 +2,9 @@
 from src.mine.creditCard import pay_computes as pc
 
 # 2020.01
-my = 0
-pf = 0
-zs = 0
+my = 1904.2
+pf = 1637.8
+zs = 2136.7
 ms = 0
 pay01 = pc.need_to_pay('2020.01', my, pf, zs, ms)
 
