a1a23425e8b25b796e50e9abc15ef6a69d1bb2a4
yangheng <yangheng@xinye.com>
2021-07-08 14:22:34 +0800

    add 202105 pay

--- src/mine/creditCard/2021_credit_card.py
+++ src/mine/creditCard/2021_credit_card.py
@@ -32,8 +32,8 @@ pay04 = pc.need_to_pay('2021.04', my, pf, zs, ms)
 # 2021.05
 ms = 0
 my = 0
-zs = 0
-pf = 0
+zs = 3709.83
+pf = 1810.98
 pay05 = pc.need_to_pay('2021.05', my, pf, zs, ms)
 
 # 2021.06
