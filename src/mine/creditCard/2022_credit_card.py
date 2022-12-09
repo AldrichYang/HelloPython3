b89aeadf8be5edec811374c4f0b9940f30b19e00
yangheng <yangheng@xinye.com>
2022-12-09 11:26:59 +0800

    add 202211 pay

--- src/mine/creditCard/2022_credit_card.py
+++ src/mine/creditCard/2022_credit_card.py
@@ -74,8 +74,8 @@ pay10 = pc.need_to_pay('2022.10', my, pf, zs, ms)
 # 2022.11
 ms = 0
 my = 0
-zs = 0
-pf = 0
+zs = 4107.55
+pf = 14783.54
 pay11 = pc.need_to_pay('2022.11', my, pf, zs, ms)
 
 # 2022.12
