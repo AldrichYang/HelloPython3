53f0fdd49b68394fe73b5957ead9a88b0c0e6ff7
Aldrich Yang <yangheng_forever@163.com>
2025-03-10 17:34:08 +0800

    remove unused modules and scripts from the project

--- src/mine/creditCard/pay_computes.py
+++
@@ -1,5 +0,0 @@
-def need_to_pay(yearmonth, my=0, pf=0, zs=0, ms=0, jd=0):
-    unpay = my + pf + zs + ms + jd
-    # format数字格式化
-    print(yearmonth + ' credit card cost: ', format(unpay, '0.2f'))
-    return unpay
